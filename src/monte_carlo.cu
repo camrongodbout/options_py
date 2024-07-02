740813ef2ed40e889a8f8e9f0945b5b593876536
Camron Godbout <camrongodbout@users.noreply.github.com>
2024-07-02 17:55:28 -0400

    update thread size

--- src/monte_carlo.cu
+++ src/monte_carlo.cu
@@ -36,14 +36,14 @@ __global__ void monte_carlo_kernel(float *d_paths, float *d_payoffs, curandState
 }
 
 void monte_carlo_simulation(float *d_paths, float *d_payoffs, curandState *state, int n_paths, int n_steps, float s, float t, float r, float sigma, float dt, float k, bool is_call) {
-  int threads = 256;
+  int threads = 1024;
   int blocks = (n_paths + threads - 1) / threads;
   size_t shared_memory_size = threads * sizeof(float);
   monte_carlo_kernel<<<blocks, threads, shared_memory_size>>>(d_paths, d_payoffs, state, n_paths, n_steps, s, t, r, sigma, dt, k, is_call);
 }
 
 void init_curand_states(curandState *state, int n_paths, unsigned long long seed) {
-  int threads = 256;
+  int threads = 1024;
   int blocks = (n_paths + threads - 1) / threads;
   init_curand_kernel<<<blocks, threads>>>(state, seed, n_paths);
 }
